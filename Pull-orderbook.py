57105e981ed205b34190cb01a5e299c20062d8af
richrha <richrha@naver.com>
2023-05-25 19:02:51 +0900

    Update Pull-orderbook.py
    Added code for pulling trade book. Rearranged functions.

--- Pull-orderbook.py
+++ Pull-orderbook.py
@@ -37,31 +37,115 @@ def default_settings():
 
     return setting
 
-def error_message(timestamp):
+def error_message(timestamp, type):
     File = open('./raw/error_log.txt', 'a')
-    File.write("Error has occured in " + timestamp.strftime('%Y-%m-%d %H:%M:%S') + "\n")
-    File.close()
+    if type == 0:
+        File.write("An orderbook error has occurred in " + timestamp.strftime('%Y-%m-%d %H:%M:%S') + "\n")
+    else:
 
+        File.write("An tradebook error has occurred in " + timestamp.strftime('%Y-%m-%d %H:%M:%S') + "\n")
+    File.close()
 
 def write_csv(data, timestamp):
     date = timestamp.strftime('%Y-%m-%d')
 
     #File name: Date_market_orderbook.csv
-    csv_directory = "./raw/" + date + "-" + market + '-' + currency + "-orderbook.csv"
+    csv_directory = ["./raw/" + date + "-" + market + '-' + currency + "-orderbook.csv", 
+                     "./raw/" + date + "-" + market + '-' + currency + "-tradebook.csv"]
 
     if not os.path.isdir('./raw'):
         os.makedirs('./raw')
 
     #Save header when file doesn't exists
-    should_write_header = os.path.exists(csv_directory)
+    for i in {0, 1}:
+        should_write_header = os.path.exists(csv_directory[i])
+        if should_write_header == False:
+            data[i].to_csv(csv_directory[i], index=False, header=True, mode = 'a')
+        else:
+            data[i].to_csv(csv_directory[i], index=False, header=False, mode = 'a')
 
-    if should_write_header == False:
-        data.to_csv(csv_directory, index=False, header=True, mode = 'a')
-    else:
-        data.to_csv(csv_directory, index=False, header=False, mode = 'a')
+def get_orderbook_tradebook(market, url_dictionary, timestamp):
 
-def pull_csv_orderbook():
+    #request orderbook
+    try: 
+        orderbook_response = requests.get(url_dictionary[market][0]).json()
+        orderbook = orderbook_response['data']
+    except:
+        error_message(timestamp, 0)
+        orderbook = None
 
+    #request tradebook
+    try: 
+        tradebook_response = requests.get(url_dictionary[market][1]).json()
+        tradebook = tradebook_response['data']
+    except:
+        error_message(timestamp, 1)
+        tradebook = None
+
+    return orderbook, tradebook
+    
+def reprocess_orderbook(orderbook, request_time):
+
+    ##bid = buy
+    bids = (pd.DataFrame(orderbook['bids'])).apply(pd.to_numeric, errors='ignore')
+    bids.sort_values('price', ascending=False, inplace=True) ##ascending
+    bids = bids.reset_index(); del bids['index']
+    bids['type'] = 0
+
+    ##ask = sell
+    asks = (pd.DataFrame(orderbook['asks'])).apply(pd.to_numeric, errors='ignore')
+    asks.sort_values('price', ascending=True, inplace=True) ##descending
+    asks = asks.reset_index(); del asks['index']
+    asks['type'] = 1
+
+    #rearranged orderbook
+    new_orderbook = pd.concat([bids, asks])
+    new_orderbook['quantity'] = new_orderbook['quantity'].round(decimals=4)
+    new_orderbook['timestamp'] = request_time
+    
+    """
+    new_orderbook format:
+            Price             |   quantity   |type |  timestamp
+    Bid 1 : Lowest price      |   quantity   |  0  |  timestamp
+    Bid 2 : 2nd lowest price  |   quantity   |  0  |  timestamp
+        ...  :        ...        |      ...     | ... |     ...
+    Bid n : Highest price     |   quantity   |  0  |  timestamp
+    Ask 1 : Highest price     |   quantity   |  1  |  timestamp
+    Ask 2 : 2nd highest price |   quantity   |  1  |  timestamp
+        ...  :        ...        |      ...     | ... |     ...
+    Ask n :Lowest price       |   quantity   |  1  |  timestamp
+    """
+    
+    return new_orderbook
+
+def reprocess_tradebook(tradebook):
+    
+    global previous_tradebook
+
+    tradebook = pd.DataFrame(tradebook).apply(pd.to_numeric, errors='ignore')
+
+    #change type to 0 and 1
+    tradebook.loc[tradebook['type'] == 'bid', 'type'] = 0
+    tradebook.loc[tradebook['type'] == 'ask', 'type'] = 1 
+
+    tradebook.sort_values('transaction_date', ascending = True, inplace = True) ## ascending
+    
+    if previous_tradebook.empty:
+        previous_tradebook = tradebook
+        return tradebook
+    
+    #extract new trade information rows only
+    test = pd.merge(previous_tradebook, tradebook, how = 'outer', indicator = True)
+    test = test.query("_merge == 'right_only'")
+    test = test.drop(columns = ['_merge'])
+
+    previous_tradebook = tradebook
+    tradebook = test
+
+    return tradebook
+
+def pull_csv_orderbook_tradebook():
+    
     timestamp = datetime.now()
     time_interval = 0
 
@@ -73,51 +157,15 @@ def pull_csv_orderbook():
 
         timestamp = datetime.now()
         request_time = timestamp.strftime('%Y-%M-%D %H:%M:%S')
-        
-        try: 
-            response = requests.get(url_dictionary[market])
-        except:
-            error_message(timestamp)
-            continue
-        orderbook = response.json()
-        
-        data = orderbook['data']
-        
-        ##bid = buy
-        bids = (pd.DataFrame(data['bids'])).apply(pd.to_numeric, errors='ignore')
-        bids.sort_values('price', ascending=False, inplace=True) ##ascending
-        bids = bids.reset_index(); del bids['index']
-        bids['type'] = 0
 
-        ##aks = sell
-        asks = (pd.DataFrame(data['asks'])).apply(pd.to_numeric, errors='ignore')
-        asks.sort_values('price', ascending=True, inplace=True) ##descending
-        asks = asks.reset_index(); del asks['index']
-        asks['type'] = 1
+        orderbook, tradebook = get_orderbook_tradebook(market, url_dictionary, timestamp)
 
-        #rearranged orderbook
-        new_orderbook = pd.concat([bids, asks])
-        new_orderbook['quantity'] = new_orderbook['quantity'].round(decimals=4)
-        new_orderbook['timestamp'] = request_time
+        orderbook = reprocess_orderbook(orderbook, request_time)
+        tradebook = reprocess_tradebook(tradebook)
 
-        """
-        new_orderbook format:
-                Price             |   quantity   |type |  timestamp
-        Bid 1 : Lowest price      |   quantity   |  0  |  timestamp
-        Bid 2 : 2nd lowest price  |   quantity   |  0  |  timestamp
-         ...  :        ...        |      ...     | ... |     ...
-        Bid n : Highest price     |   quantity   |  0  |  timestamp
-        Ask 1 : Highest price     |   quantity   |  1  |  timestamp
-        Ask 2 : 2nd highest price |   quantity   |  1  |  timestamp
-         ...  :        ...        |      ...     | ... |     ...
-        Ask n :Lowest price       |   quantity   |  1  |  timestamp
-        """
+        data = [orderbook, tradebook]
 
-        #print(new_orderbook)
-
-        write_csv(new_orderbook, timestamp)
-
-
+        write_csv(data, timestamp)
 
 market_list = ['Bithumb', 'Upbit' ]
 currency_list = ['BTC', 'ETH', 'USDT']
@@ -127,17 +175,18 @@ def main():
     global market
     global currency 
     global url_dictionary
+    global previous_tradebook
 
+    previous_tradebook = pd.DataFrame()
     initial_setting = default_settings()
     market = initial_setting["market"]
     currency = initial_setting["currency"]
-    print(market)
-    print(currency)
 
-    url_dictionary = {'Bithumb':'https://api.bithumb.com/public/orderbook/%s_KRW/?count=10' %currency, 
-                'Upbit':"'https://api.upbit.com/v1/orderbook?markets=KRW-%s' % currency"}
+    url_dictionary = {'Bithumb':['https://api.bithumb.com/public/orderbook/%s_KRW/?count=10' %currency, 
+                                 'https://api.bithumb.com/public/transaction_history/%s_KRW/?count=50' % currency], 
+                      'Upbit':['https://api.upbit.com/v1/orderbook?markets=KRW-%s' % currency,
+                                ]}
 
-    pull_csv_orderbook()
+    pull_csv_orderbook_tradebook()
 
-    
 main()
