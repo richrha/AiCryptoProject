ff2c00312be9791ac958544e13b82e2ea0c9ea85
richrha <richrha@naver.com>
2023-06-04 15:57:27 +0900

    Update Orderbook-Feature-calculator.py

--- Orderbook-Feature-calculator.py
+++ Orderbook-Feature-calculator.py
@@ -40,11 +40,11 @@ def top_price_calculator(row, count):
     if (count)%level == 0: #top row of bid or ask
         if row.iloc[0, 2] == 0: #bid
             top_price[0] = row.iloc[0,0]
-            History.iloc[1, 2] = top_price[0]
+            History.iloc[0, 2] = top_price[0]
         
         if row.iloc[0, 2] == 1: #ask
             top_price[1] = row.iloc[0,0]
-            History.iloc[1,3] = top_price[1]
+            History.iloc[0,3] = top_price[1]
 
 def mean_price_calculator(row, count):
     
@@ -113,6 +113,11 @@ def mean_quantity_calculator(row, count):
         mean_quantity[1] = mean_quantity[1] + row.iloc[0,1]
     
     if (count+1)%(level*2) == 0 and count != 0: #at each point when a group ends
+        #giving sum of bid & ask quantity for Book D
+        History.iloc[0, 0] = mean_quantity[0]
+        History.iloc[0, 1] = mean_quantity[1]
+
+        #calculating each mean quantity
         mean_quantity[0] = mean_quantity[0] / level
         mean_quantity[1] = mean_quantity[1] / level
 
@@ -152,12 +157,10 @@ def book_imbalance_calculator(row, count, ratio, interval):
     if row.iloc[0,2] == 0: #bid
         bidQty = bidQty + row.iloc[0,1] ** ratio
         bidPx = bidPx + row.iloc[0,0] * bidQty
-        History.iloc[1,0] = bidQty
 
     if row.iloc[0,2] == 1: #ask
         askQty = askQty + row.iloc[0,1] ** ratio
         askPx = askPx + row.iloc[0,0] * askQty
-        History.iloc[1,1] = askQty
     
     if (count+1)%(level*2) == 0 and count != 0: #at each point when a group ends
         book_price = (((askQty*bidPx)/bidQty) + ((bidQty*askPx)/askQty))/(bidQty + askQty)
@@ -202,27 +205,19 @@ def mid_price_market_mid(count):
         mid_price_val = (mid_price[0] * mid_quantity[1] + mid_price[1] * mid_quantity[0]) / (mid_quantity[0] + mid_quantity[1])
         features['mid_price_market_mid'] = mid_price_val
          
-def bookv_calculator(count):
-
-    if (count + 1)%(level*2) == 0 and count != 0: #at each point when a group ends
-        if History.iloc[0,0] == 0:
-            History.iloc[0] = History.iloc[1]
-
-    
-
+def write_timestamp(row):
+    features['timestamp'] = row.iloc[0, 3]
+    History['timestamp'] = row.iloc[0, 3]
 
 def main():
 
-    global headname
-    global group_n, level
+    global level
     global top_price, mean_price, mid_price
     global top_quantity, mean_quantity, mid_quantity
 
-    global features
+    global features, History
 
-    global History, grad
-
-    History = pd.DataFrame({'bidQty':[0, 0], 'askQty':[0, 0], 'bidTop':[0, 0], 'askTop':[0, 0]}, index = ['0', '1'])
+    History = pd.DataFrame({'bidQty':[0], 'askQty':[0], 'bidTop':[0], 'askTop':[0], 'timestamp':[0]})
 
     features = pd.DataFrame({}, index = ['0'])
 
@@ -230,7 +225,7 @@ def main():
     top_quantity, mean_quantity, mid_quantity = [0,0], [0,0], [0,0]
 
     file = ['./raw/2023-05-25-Bithumb-BTC-orderbook.csv', './raw/2023-05-25-Bithumb-BTC-tradebook.csv']
-    file[0] = input('type file name: ')
+    #file[0] = input('type file name: ')
     try:
         headname = "-".join("".join(file[0].split('/')[2]).split('-')[0:5])
     except:
@@ -263,11 +258,14 @@ def main():
 
         book_imbalance_calculator(orderbook_row, i, 0.5, 1)
 
-        #bookv_calculator()
+        write_timestamp(orderbook_row)
 
-        if (i + 1)%(level*2) == 0 and i != 0: 
-            write_csv(features, headname+'-features.csv')
+        if (i + 1)%(level*2) == 0 and i != 0: #at each end of the time stamp group
+            write_csv(features, headname+'-orderbook-features.csv')
+            write_csv(History, headname+'-BQ-AQ-BT-AT.csv')
 
+            for i in range(5): #reset History
+                History.iloc[0, i] = 0
 
 
 main()
